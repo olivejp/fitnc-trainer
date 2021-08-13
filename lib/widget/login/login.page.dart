942a269621435726d606cb270c097cadc1618331
olivejp <jean-paul.olive@deveo.nc>
2021-08-13 15:24:27 +1100

    Login

--- lib/widget/login/login.page.dart
+++ lib/widget/login/login.page.dart
@@ -11,7 +11,7 @@ import '../bottom.widget.dart';
 typedef CallbackUserCredential = void Function(UserCredential userCredential);
 
 class LoginPage extends StatefulWidget {
-  LoginPage({Key? key,  this.callback}) : super(key: key);
+  LoginPage({Key? key, this.callback}) : super(key: key);
 
   final LoginBloc bloc = LoginBloc.getInstance();
   final CallbackUserCredential? callback;
@@ -22,7 +22,7 @@ class LoginPage extends StatefulWidget {
   }
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
   _LoginPageState();
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
@@ -35,147 +35,159 @@ class _LoginPageState extends State<LoginPage> {
     );
 
     return Scaffold(
-      backgroundColor: FitnessNcColors.blue50,
+        backgroundColor: FitnessNcColors.blue50,
         body: Stack(children: <Widget>[
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 500),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 30, left: 20),
-                    child: Text(
-                      'Fitness Nc',
-                      style: GoogleFonts.alfaSlabOne(color: Theme.of(context).primaryColor, fontSize: 25),
-                    ),
-                  ),
-                  Card(
-                    elevation: 20,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(60.0),
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(
-                              'Connectez-vous à votre compte',
-                              style: GoogleFonts.robotoTextTheme().headline6!.copyWith(fontSize: 28, fontWeight: FontWeight.bold),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 30),
-                              child: TextFormField(
-                                style: GoogleFonts.roboto(fontSize: 15),
-                                decoration: InputDecoration(
-                                    labelText: 'Email',
-                                    hintStyle: GoogleFonts.roboto(fontSize: 15),
-                                    focusedBorder: defaultBorder,
-                                    border: defaultBorder,
-                                    enabledBorder: defaultBorder),
-                                onChanged: (String value) => widget.bloc.email = value,
-                                onFieldSubmitted: (String value) => onPressedEnter(),
-                                validator: (String? value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Merci de renseigner votre adresse email.';
-                                  }
-                                  if (!RegExp(
-                                          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
-                                      .hasMatch(value)) {
-                                    return "L'adresse mail n'est pas formatée correctement'.";
-                                  }
-                                  return null;
-                                },
-                                textInputAction: TextInputAction.done,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 30),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: <Widget>[
-                                  TextFormField(
-                                    style: GoogleFonts.roboto(fontSize: 15),
-                                    obscureText: _vnHidePassword.value,
-                                    enableSuggestions: false,
-                                    autocorrect: false,
-                                    decoration: InputDecoration(
-                                        labelText: 'Mot de passe',
-                                        focusedBorder: defaultBorder,
-                                        border: defaultBorder,
-                                        enabledBorder: defaultBorder,
-                                        hintStyle: GoogleFonts.roboto(fontSize: 15),
-                                        suffixIcon: IconButton(
-                                            tooltip: _vnHidePassword.value ? 'Afficher le mot de passe' : 'Masquer le mot de passe',
-                                            onPressed: () => setState(() => _vnHidePassword.value = !_vnHidePassword.value),
-                                            icon: ValueListenableBuilder<bool>(
-                                              builder: (BuildContext context, bool value, Widget? child) {
-                                                return value ? const Icon(Icons.visibility_outlined) : const Icon(Icons.visibility_off_outlined);
-                                              },
-                                              valueListenable: _vnHidePassword,
-                                            ))),
-                                    onChanged: (String value) => widget.bloc.password = value,
-                                    onFieldSubmitted: (_) => onPressedEnter(),
-                                  ),
-                                  TextButton(
-                                      onPressed: () => print('hello'),
-                                      child: const Text(
-                                        'Mot de passe oublié ?',
-                                      )),
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 30),
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(minimumSize: const Size(double.infinity, 55)),
-                                onPressed: onPressedEnter,
-                                child: Text(
-                                  'Continuer',
-                                  style: GoogleFonts.roboto(color: Color(Colors.white.value), fontSize: 15),
-                                ),
-                              ),
-                            ),
-                            StreamBuilder<String?>(
-                              stream: widget.bloc.errorsObservable,
-                              builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-                                if (snapshot.hasData && snapshot.data?.isNotEmpty == true) {
-                                  return Text(snapshot.data!);
-                                } else {
-                                  return const Text('');
-                                }
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30, left: 20),
-                    child: Row(
-                      children: <Widget>[
-                        const Text("Vous n'avez pas de compte ?"),
-                        TextButton(
-                            onPressed: () => Navigator.pushNamed(context, '/sign_up'),
-                            child: const Text(
-                              "S'incrire",
-                            )),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+          Transform(
+            transform: Matrix4.identity()
+              ..translate(MediaQuery.of(context).size.width)
+              ..add(Matrix4.skewX(-0.3)),
+            child: Container(
+              width: 200,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomLeft, end: Alignment.topRight, colors: [Colors.amber.withAlpha(100), Colors.amber.shade700])),
             ),
           ),
-        ],
-      ),
-      const BottomCu(),
-    ]));
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 500),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 30, left: 20),
+                        child: Text(
+                          'Fitness Nc',
+                          style: GoogleFonts.alfaSlabOne(color: Theme.of(context).primaryColor, fontSize: 25),
+                        ),
+                      ),
+                      Card(
+                        elevation: 20,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(60.0),
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text(
+                                  'Connectez-vous à votre compte',
+                                  style: GoogleFonts.robotoTextTheme().headline6!.copyWith(fontSize: 28, fontWeight: FontWeight.bold),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 30),
+                                  child: TextFormField(
+                                    style: GoogleFonts.roboto(fontSize: 15),
+                                    decoration: InputDecoration(
+                                        labelText: 'Email',
+                                        hintStyle: GoogleFonts.roboto(fontSize: 15),
+                                        focusedBorder: defaultBorder,
+                                        border: defaultBorder,
+                                        enabledBorder: defaultBorder),
+                                    onChanged: (String value) => widget.bloc.email = value,
+                                    onFieldSubmitted: (String value) => onPressedEnter(),
+                                    validator: (String? value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Merci de renseigner votre adresse email.';
+                                      }
+                                      if (!RegExp(
+                                              r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                                          .hasMatch(value)) {
+                                        return "L'adresse mail n'est pas formatée correctement'.";
+                                      }
+                                      return null;
+                                    },
+                                    textInputAction: TextInputAction.done,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 30),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: <Widget>[
+                                      TextFormField(
+                                        style: GoogleFonts.roboto(fontSize: 15),
+                                        obscureText: _vnHidePassword.value,
+                                        enableSuggestions: false,
+                                        autocorrect: false,
+                                        decoration: InputDecoration(
+                                            labelText: 'Mot de passe',
+                                            focusedBorder: defaultBorder,
+                                            border: defaultBorder,
+                                            enabledBorder: defaultBorder,
+                                            hintStyle: GoogleFonts.roboto(fontSize: 15),
+                                            suffixIcon: IconButton(
+                                                tooltip: _vnHidePassword.value ? 'Afficher le mot de passe' : 'Masquer le mot de passe',
+                                                onPressed: () => setState(() => _vnHidePassword.value = !_vnHidePassword.value),
+                                                icon: ValueListenableBuilder<bool>(
+                                                  builder: (BuildContext context, bool value, Widget? child) {
+                                                    return value ? const Icon(Icons.visibility_outlined) : const Icon(Icons.visibility_off_outlined);
+                                                  },
+                                                  valueListenable: _vnHidePassword,
+                                                ))),
+                                        onChanged: (String value) => widget.bloc.password = value,
+                                        onFieldSubmitted: (_) => onPressedEnter(),
+                                      ),
+                                      TextButton(
+                                          onPressed: () => print('hello'),
+                                          child: const Text(
+                                            'Mot de passe oublié ?',
+                                          )),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 30),
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(minimumSize: const Size(double.infinity, 55)),
+                                    onPressed: onPressedEnter,
+                                    child: Text(
+                                      'Continuer',
+                                      style: GoogleFonts.roboto(color: Color(Colors.white.value), fontSize: 15),
+                                    ),
+                                  ),
+                                ),
+                                StreamBuilder<String?>(
+                                  stream: widget.bloc.errorsObservable,
+                                  builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+                                    if (snapshot.hasData && snapshot.data?.isNotEmpty == true) {
+                                      return Text(snapshot.data!);
+                                    } else {
+                                      return const Text('');
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30, left: 20),
+                        child: Row(
+                          children: <Widget>[
+                            const Text("Vous n'avez pas de compte ?"),
+                            TextButton(
+                                onPressed: () => Navigator.pushNamed(context, '/sign_up'),
+                                child: const Text(
+                                  "S'incrire",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const BottomCu(),
+        ]));
   }
 
   void onPressedEnter() {
