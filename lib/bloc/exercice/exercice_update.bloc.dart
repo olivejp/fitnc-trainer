b4325cac5b07a9b837f844bc6277fec6bcba32ec
orlanth23 <orlanth23@hotmail.com>
2021-08-18 00:39:52 +1100

    Refacto et ajout de la gestion des abonnés.

--- lib/bloc/exercice/exercice_update.bloc.dart
+++ lib/bloc/exercice/exercice_update.bloc.dart
@@ -6,13 +6,10 @@ import 'package:fitnc_trainer/core/bloc/generic.bloc.dart';
 import 'package:fitnc_trainer/domain/exercice.domain.dart';
 import 'package:fitnc_trainer/service/param.service.dart';
 import 'package:fitnc_trainer/service/trainers.service.dart';
-import 'package:fitnc_trainer/widget/exercice/exercice.form.builder.dart';
 import 'package:fitnc_trainer/widget/widgets/storage_image.widget.dart';
-import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
-class ExerciceUpdateBloc extends AbstractFitnessCrudBloc<Exercice>
-    with MixinFitnessStorageBloc<Exercice> {
+class ExerciceUpdateBloc extends AbstractFitnessCrudBloc<Exercice> with MixinFitnessStorageBloc<Exercice> {
   ExerciceUpdateBloc._();
 
   factory ExerciceUpdateBloc.instance() {
@@ -25,18 +22,15 @@ class ExerciceUpdateBloc extends AbstractFitnessCrudBloc<Exercice>
   TrainersService trainersService = TrainersService.instance();
   ParamService paramService = ParamService.getInstance();
   Exercice? exercice;
+  bool sendStorage = false;
 
-  final BehaviorSubject<StorageFile?> subjectStoragePair =
-      BehaviorSubject<StorageFile?>();
-  final BehaviorSubject<String?> _streamSelectedVideoUrl =
-      BehaviorSubject<String?>();
-  final BehaviorSubject<String?> _streamSelectedYoutubeUrl =
-      BehaviorSubject<String?>();
+  final BehaviorSubject<StorageFile?> subjectStoragePair = BehaviorSubject<StorageFile?>();
+  final BehaviorSubject<String?> _streamSelectedVideoUrl = BehaviorSubject<String?>();
+  final BehaviorSubject<String?> _streamSelectedYoutubeUrl = BehaviorSubject<String?>();
 
   Stream<String?>? get selectedVideoUrlObs => _streamSelectedVideoUrl.stream;
 
-  Stream<String?>? get selectedYoutubeUrlObs =>
-      _streamSelectedYoutubeUrl.stream;
+  Stream<String?>? get selectedYoutubeUrlObs => _streamSelectedYoutubeUrl.stream;
 
   Stream<StorageFile?> get obsStoragePair => subjectStoragePair.stream;
 
@@ -63,8 +57,10 @@ class ExerciceUpdateBloc extends AbstractFitnessCrudBloc<Exercice>
     if (exerciceEntered != null) {
       exercice = exerciceEntered;
       exercice!.storageFile = StorageFile();
-      getFutureStorageFile(exercice!)
-          .then((StorageFile? value) => subjectStoragePair.sink.add(value));
+      getFutureStorageFile(exercice!).then((StorageFile? value) {
+        exercice!.storageFile = value;
+        subjectStoragePair.sink.add(value);
+      });
 
       if (exercice!.videoUrl != null) {
         videoUrl = exercice!.videoUrl;
@@ -77,15 +73,21 @@ class ExerciceUpdateBloc extends AbstractFitnessCrudBloc<Exercice>
   }
 
   Future<void> saveExercice() async {
-    if (exercice != null) {
-      if (exercice!.uid != null) {
+    if (exercice == null) {
+      return;
+    }
+
+    bool isUpdate = exercice!.uid != null;
+    if (isUpdate) {
+      if (sendStorage) {
         return eraseAndReplaceStorage(exercice!).then((_) => save(exercice!));
       } else {
-        exercice!.uid = getCollectionReference().doc().id;
-        return createStorage(exercice!).then((_) => create(exercice!));
+        return save(exercice!);
       }
+    } else {
+      exercice!.uid = getCollectionReference().doc().id;
+      return createStorage(exercice!).then((_) => create(exercice!));
     }
-    return;
   }
 
   set typeExercice(String? value) {
@@ -121,6 +123,7 @@ class ExerciceUpdateBloc extends AbstractFitnessCrudBloc<Exercice>
   String? get youtubeUrl => exercice?.youtubeUrl;
 
   void setStoragePair(StorageFile? storageFile) {
+    sendStorage = true;
     exercice?.storageFile = storageFile;
     subjectStoragePair.sink.add(exercice?.storageFile);
   }
