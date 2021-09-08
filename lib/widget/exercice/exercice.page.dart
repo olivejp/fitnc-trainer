15a7d8e69f8d7123d2aeeb0b012ea4943a97e0d4
orlanth23 <orlanth23@hotmail.com>
2021-09-09 08:32:23 +1100

    Test de fix en passant par un StreamBuilder.

--- lib/widget/exercice/exercice.page.dart
+++ lib/widget/exercice/exercice.page.dart
@@ -167,8 +167,6 @@ class _ExerciceListSearchState extends State<_ExerciceListSearch> {
       UtilService.search(_query, listCompleteExercice, _streamListExercice);
     });
 
-    return Text('test');
-
     return Column(
       children: <Widget>[
         Padding(
